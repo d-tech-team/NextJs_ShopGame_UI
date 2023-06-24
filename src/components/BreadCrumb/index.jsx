2c7e2852d60be70a3f8366c42d4148d1f5d6d702
Le Tien Dat <lctiendat@gmail.com>
2023-06-24 12:23:18 +0700

    change ts to js

---
+++ src/components/BreadCrumb/index.jsx
@@ -0,0 +1,13 @@
+import { Breadcrumb } from "react-bootstrap"
+import styles from './index.module.scss'
+import Link from "next/link"
+
+
+export default ({ data  }) => {
+    return <Breadcrumb className={styles.BreadCrumb}>
+        <Breadcrumb.Item className={styles.BreadCrumb__item}> <Link href={'/'}> Trang chủ</Link> </Breadcrumb.Item>
+        {
+            Array.isArray(data) && data.map((obj, i) => <Breadcrumb.Item key={i} className={styles.BreadCrumb__item}> <Link href={obj.path}> {obj.label}</Link> </Breadcrumb.Item>)
+        }
+    </Breadcrumb>
+} 
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,6 +1,7 @@
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
-export default ({ images }) => {
+
+export default  ({ images }) => {
     images = [
         {
             original: 'https://picsum.photos/id/1018/1000/600/',
